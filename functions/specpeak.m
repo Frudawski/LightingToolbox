f95f71e5411ef434b44aee3a375b1c2fcdbfd7f8
Frederic Rudawski <f.rudawski@googlemail.com>
2023-09-30 10:51:57 +0200

    added special cases handling and fixed minor bug

--- functions/specpeak.m
+++ functions/specpeak.m
@@ -5,19 +5,22 @@
 %
 % Author: Frederic Rudawski
 % Date: 29.04.2023 (Saturday)
+% last updated: 30.09.2023
 
 function sp = specpeak(lam,spec)
 
-% find peak wavelengths
-[~,idx] = max(spec,[],2);
+sp = NaN(size(spec,1),1);
 
 % check lambda matrix size
 if size(lam,1)==1 && size(spec,1)>1
     lam = repmat(lam,size(spec,1),1);
 end
 
-% return peak wavelengths
-sp = NaN(size(spec,1),1);
 for n = 1:size(spec,1)
-    sp(n) = lam(n,idx(n));
+    % find peak wavelengths
+    v = max(spec(n,:),[],2);
+    idx = spec(n,:)==v; % allows for multiple idx with same value, [v,idx] = max() don't
+
+    % return peak wavelengths
+    sp(n) = mean(lam(n,idx));
 end
